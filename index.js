c452a65bc279eb4bf9ea6b093229c1bc470ae358
Fred Chien <cfsghost@gmail.com>
2017-03-29 07:21:06 +0800

    fixed bugs

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var TimeWindow = require('./lib/timewindow');
 
 module.exports = {
 	createTimeWindow: function(timespan, opts) {
-		return new TimeWindow(timespan.opts);
+		return new TimeWindow(timespan, opts);
 	}
 };
